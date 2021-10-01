ff338029dce08636ac74a2c31ad5b75756845f56
GiftXXVI <gift.chimphonda@outlook.com>
2021-10-01 05:06:20 +0200

    Completed Project Setup and Wrote Initial Code

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@ pip3 install flask_cors
 pip3 install flask --upgrade
 pip3 uninstall flask-socketio -y
 service postgresql start
-su - postgres bash -c "psql < /home/workspace/backend/setup.sql"
-su - postgres bash -c "psql bookshelf < /home/workspace/backend/books.psql"
+su - postgres bash -c "psql < /home/giftxxvi/Bookshelf/backend/setup.sql"
+su - postgres bash -c "psql bookshelf < /home/giftxxvi/Bookshelf/backend/books.psql"

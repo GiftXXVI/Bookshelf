73b5944611fac0b9abb2f5415b67ce6fb14aafd3
GiftXXVI <gift.chimphonda@outlook.com>
2021-10-03 09:47:51 +0200

    Implemented Error Routes

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -45,14 +45,14 @@ def create_app(test_config=None):
     @app.route('/books', methods=['GET'])
     def get_books():
         page = request.args.get('page', 1, type=int)
-        start = (page - 1) * 10
-        end = start + 10
+        start = (page - 1) * BOOKS_PER_SHELF
+        end = start + BOOKS_PER_SHELF
 
         try:
             books = Book.query.order_by(Book.id).all()
             formatted_books = [book.format() for book in books]
         except:
-            abort(500)
+            abort(422)
         finally:
             if len(formatted_books[start:end]) == 0:
                 abort(404)
@@ -73,7 +73,7 @@ def create_app(test_config=None):
                                 'book': book.format,
                                 'total_books': 1})
         except:
-            abort(400)
+            abort(422)
 
     # @TODO: Write a route that will update a single book's rating.
     #         It should only be able to update the rating, not the entire representation
@@ -99,6 +99,7 @@ def create_app(test_config=None):
         except:
             db.session.rollback()
             error = True
+
         finally:
             db.session.close()
             if not error:
@@ -107,7 +108,7 @@ def create_app(test_config=None):
                     'id': book_id
                 })
             else:
-                abort(400)
+                abort(422)
 
     # @TODO: Write a route that will delete a single book.
     #        Response body keys: 'success', 'deleted'(id of deleted book), 'books' and 'total_books'
@@ -121,39 +122,41 @@ def create_app(test_config=None):
         try:
             book = Book.query.filter_by(id=book_id).one_or_none()
 
-            if book is None:
-                abort(404)
-
-            book.delete()
+            if book is not None:
+                book.delete()
         except:
             db.session.rollback()
+            abort(422)
         finally:
             db.session.close()
 
-            selection = Book.query.order_by(Book.id).all()
-            formatted_books = [item.format() for item in selection]
-            total_books = len(formatted_books)
-            current_books = []
+            if book is not None:
+                selection = Book.query.order_by(Book.id).all()
+                formatted_books = [item.format() for item in selection]
+                total_books = len(formatted_books)
+                current_books = []
 
-            previous_books = Book.query.filter(Book.id < book_id).count()
-            page = int(previous_books/10) + 1
-            start = (page - 1) * 10
-            end = start + 10
+                previous_books = Book.query.filter(Book.id < book_id).count()
+                page = int(previous_books/10) + 1
+                start = (page - 1) * BOOKS_PER_SHELF
+                end = start + BOOKS_PER_SHELF
 
-            if(len(formatted_books[start:end]) > 0):
-                current_books = formatted_books[start:end]
+                if(len(formatted_books[start:end]) > 0):
+                    current_books = formatted_books[start:end]
 
-            return jsonify({
-                'success': True,
-                'deleted': book_id,
-                'books': current_books,
-                'total_books': total_books
-            })
-
+                return jsonify({
+                    'success': True,
+                    'deleted': book_id,
+                    'books': current_books,
+                    'total_books': total_books
+                })
+            else:
+                abort(404)
     # @TODO: Write a route that create a new book.
     #        Response body keys: 'success', 'created'(id of created book), 'books' and 'total_books'
     # TEST: When completed, you will be able to a new book using the form. Try doing so from the last page of books.
     #       Your new book should show up immediately after you submit it at the end of the page.
+
     @app.route('/books', methods=['POST'])
     def create_book():
         body = request.get_json()
@@ -174,8 +177,8 @@ def create_app(test_config=None):
             selection = Book.query.order_by(Book.id).all()
             formatted_books = [item.format() for item in selection]
             page = int(len(formatted_books)/10) + 1
-            start = (page - 1) * 10
-            end = start + 10
+            start = (page - 1) * BOOKS_PER_SHELF
+            end = start + BOOKS_PER_SHELF
             total_books = len(formatted_books)
         print(book.id, file=sys.stderr)
         return jsonify({
@@ -184,6 +187,21 @@ def create_app(test_config=None):
             'books': formatted_books[start:end],
             'total_books': total_books
         })
-        return app
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            'success': False,
+            'error': 404,
+            'message': 'Not Found.'
+        }), 404
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            'success': False,
+            'error': 422,
+            'message': 'unprocessable'
+        }), 422
 
     return app
